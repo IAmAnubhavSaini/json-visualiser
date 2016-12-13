93663d216d673d8357ed84f7192ca84a8af8c839
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-13 13:49:39 +0530

    This is better.
    People tell me that this code is very sexy.  Everyone says so.
    
    I feel the code is sexy; so the code is sexy.

--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,14 @@
 (function (app) {
+  'use strict';
 
-  var jsonMix = app.DATA.jsonMix;
-  var jsonSample = app.DATA.jsonSample;
-  var jsonComplexArray = app.DATA.jsonComplexArray;
-  var jsonAll = app.DATA.jsonAll = [ jsonMix, jsonSample, jsonComplexArray ];
-  var createFullMap = app.ACTIONS.createFullMap;
-  var getJSObject = function (json) { return JSON.parse(json); }
-  var getObjectKeys = function (obj) { return Object.keys(obj); }
-  var generateFullMap = function(key, obj, initialLevel) { return createFullMap(obj, key, initialLevel); };
-  var writeOnDocument = function (keys, obj, initialLevel) { 
-    document.write(keys.map(function(key) {
-      return generateFullMap(key, obj, initialLevel);
-    }).join(''));
-  };
-  jsonAll.forEach(function(json, i, a) {
-    var obj = getJSObject(json);
-    var keys = getObjectKeys(obj);
-    var initialLevel = 0;
-    writeOnDocument(keys, obj, initialLevel);
+  var initialLevel = 0;
+  var jsonAll = app.DATA.jsonAll = [ app.DATA.jsonMix, app.DATA.jsonSample, app.DATA.jsonComplexArray ];
+  
+  jsonAll.map(JSON.parse).forEach(function(obj) {
+    var dataToWrite = Object.keys(obj).map(function(key) {
+      return app.ACTIONS.createFullMap(obj, key, initialLevel)
+    }).join('');
+    document.write(dataToWrite);
   });  
 
 })(window.OM || (window.OM = {}));
