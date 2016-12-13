2a3ac5f7838a4f4268645649daa23aaab81e942b
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-13 14:10:30 +0530

    Appending to page instead of writing to it.

--- scripts/run.js
+++ scripts/run.js
@@ -3,12 +3,16 @@
 
   var initialLevel = 0;
   var jsonAll = app.DATA.jsonAll = [ app.DATA.jsonMix, app.DATA.jsonSample, app.DATA.jsonComplexArray ];
-  
+  var addToPage = function(data) {
+    var div = document.createElement('div');
+    div.innerHTML = data;
+    document.getElementById('writable').appendChild(div);
+  }
   jsonAll.map(JSON.parse).forEach(function(obj) {
     var dataToWrite = Object.keys(obj).map(function(key) {
       return app.ACTIONS.createFullMap(obj, key, initialLevel)
     }).join('');
-    document.write(dataToWrite);
+    addToPage(dataToWrite);
   });  
 
 })(window.OM || (window.OM = {}));
