60380c07ec5f9746dc481ae8655265f0c54aa48a
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-13 13:41:56 +0530

    Making code go boom.

--- scripts/run.js
+++ scripts/run.js
@@ -5,13 +5,19 @@
   var jsonComplexArray = app.DATA.jsonComplexArray;
   var jsonAll = app.DATA.jsonAll = [ jsonMix, jsonSample, jsonComplexArray ];
   var createFullMap = app.ACTIONS.createFullMap;
-  var initialLevel = 0;
   var getJSObject = function (json) { return JSON.parse(json); }
   var getObjectKeys = function (obj) { return Object.keys(obj); }
-  var generateFullMap = function(key) { return createFullMap(jsobj, key, initialLevel); };
-  var jsobj = getJSObject(jsonMix);
-  var keys = getObjectKeys(jsobj);
-  var writeOnDocument = function (keys) { document.write(keys.map(generateFullMap).join('')); }
-  writeOnDocument(keys);
+  var generateFullMap = function(key, obj, initialLevel) { return createFullMap(obj, key, initialLevel); };
+  var writeOnDocument = function (keys, obj, initialLevel) { 
+    document.write(keys.map(function(key) {
+      return generateFullMap(key, obj, initialLevel);
+    }).join(''));
+  };
+  jsonAll.forEach(function(json, i, a) {
+    var obj = getJSObject(json);
+    var keys = getObjectKeys(obj);
+    var initialLevel = 0;
+    writeOnDocument(keys, obj, initialLevel);
+  });  
 
 })(window.OM || (window.OM = {}));
