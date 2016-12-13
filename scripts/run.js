9179dea1b833936e2f079bc834ce5c433b9661eb
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-13 14:36:03 +0530

    Input accepting code.

--- scripts/run.js
+++ scripts/run.js
@@ -6,13 +6,26 @@
   var addToPage = function(data) {
     var div = document.createElement('div');
     div.innerHTML = data;
+    div.className = 'kv-item';
     document.getElementById('writable').appendChild(div);
   }
-  jsonAll.map(JSON.parse).forEach(function(obj) {
-    var dataToWrite = Object.keys(obj).map(function(key) {
+  var generateData = function(obj) {
+    return Object.keys(obj).map(function(key) {
       return app.ACTIONS.createFullMap(obj, key, initialLevel)
     }).join('');
-    addToPage(dataToWrite);
+  }
+  jsonAll.map(JSON.parse).map(generateData).forEach(function(data) {
+    addToPage(data);
   });  
 
+  app.ACTIONS.generate = function () {
+    var json = document.getElementById('json').value;
+    var obj = JSON.parse(json);
+    var writable = document.getElementById('writable');
+    var oldData = writable.innerHTML;
+    var newData = generateData(obj);
+    writable.innerHTML = '';
+    addToPage(newData);
+    addToPage(oldData);
+  };
 })(window.OM || (window.OM = {}));
