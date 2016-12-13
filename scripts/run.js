aeb4e794c7b5d282877cd750e4fa71aad2ad2053
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-13 13:34:07 +0530

    Reduction of lines of code

--- scripts/run.js
+++ scripts/run.js
@@ -1,33 +1,19 @@
 (function (app) {
 
   var jsonMix = app.DATA.jsonMix;
-
   var jsonSample = app.DATA.jsonSample;
-
   var jsonComplexArray = app.DATA.jsonComplexArray;
-
-  // var allJsons = [jsonMix, jsonSample, jsonComplexArray];
-
-  function getJSObject (json) {
-    return JSON.parse(json);
-  }
-
-  function getObjectKeys (obj) {
-    return Object.keys(obj);
-  }
-
+  var jsonAll = app.DATA.jsonAll = [ jsonMix, jsonSample, jsonComplexArray ];
+  var createFullMap = app.ACTIONS.createFullMap;
+  var initialLevel = 0;
+  var getJSObject = function (json) { return JSON.parse(json); }
+  var getObjectKeys = function (obj) { return Object.keys(obj); }
+  var generateFullMap = function(key) { return createFullMap(jsobj, key, initialLevel); };
   var jsobj = getJSObject(jsonMix);
-
   var keys = getObjectKeys(jsobj);
 
-  var createFullMap = app.ACTIONS.createFullMap;
-
-  var generateFullMap = function(key) {
-    return createFullMap(jsobj, key, 0);
-  };
-
-  (function writeOnDocument(k) {
-    document.write(k.map(generateFullMap).join(''));
+  (function writeOnDocument(keys) {
+    document.write(keys.map(generateFullMap).join(''));
   })(keys);
 
 })(window.OM || (window.OM = {}));
