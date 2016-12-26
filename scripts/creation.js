3704865fdde6895d9e394a6d159b97413da24c16
Anubhav Saini <iamanubhavsaini@gmail.com>
2016-12-26 15:51:15 +0530

    Adding support for booleans. Cleanup.
    Removed ; from JS files

--- scripts/creation.js
+++ scripts/creation.js
@@ -1,35 +1,46 @@
 (function (app) {
-  'use strict';
+  'use strict'
+
+  var createKey = function(key) {
+    return '<div class="key">' + key + '</div>'
+  }
 
   var createArrayItemList = function (obj, key, level) {
-    var valEl;
+    var valEl
     var list = obj[key].map(function(x, i, a) {
       valEl = createFullMap(a, i, level + 1)
-      return '<div class="list-item v">' + valEl + '</div>';
-    });
-    var val = '<div class="list">' + list.join('') + '</div>';
-    return val;
-  };
+      return '<div class="list-item v">' + valEl + '</div>'
+    })
+    var val = '<div class="list">' + list.join('') + '</div>'
+    return val
+  }
+
+  var createKeyValPair = function(level, keyElement, valueElement) {
+    return '<div class="kv l' + level + '">' +
+              keyElement +
+              '<div class="value">' + valueElement + '</div>' +
+           '</div>'
+  }
 
   var createFullMap = function (obj, key, level) {
-    var keyEl =  '<div class="key">' + key + '</div>';
-    var valEl;
-    var objKey = obj[key];
-    var typeofObjKey = typeof objKey;
-    if(typeofObjKey === typeof 0 || typeofObjKey === typeof 'string') {
-      valEl = '<span class="v">' + objKey + '</span>';
+    var keyEl = createKey(key)
+    var valEl
+    var objKey = obj[key]
+    var typeofObjKey = typeof objKey
+    if(typeofObjKey === 'number' || typeofObjKey === 'string' || typeofObjKey === 'boolean') {
+      valEl = '<span class="v">' + objKey + '</span>'
     } else if(Array.isArray(objKey)){
-      valEl = createArrayItemList(obj, key, level);
+      valEl = createArrayItemList(obj, key, level)
     } else if(typeofObjKey === typeof {}) {
       valEl = Object.keys(objKey).map(function(k) {
-        return createFullMap(objKey, k, level + 1);
-      }).join('');
+        return createFullMap(objKey, k, level + 1)
+      }).join('')
     }
-    return '<div class="kv l' + level + '">' + keyEl + '<div class="value">' + valEl + '</div></div>';
-  };
+    return createKeyValPair(level, keyEl, valEl)
+  }
 
-  app.ACTIONS = app.ACTIONS || {};
-  app.ACTIONS.createArrayItemList = createArrayItemList;
-  app.ACTIONS.createFullMap = createFullMap;
+  app.ACTIONS = app.ACTIONS || {}
+  app.ACTIONS.createArrayItemList = createArrayItemList
+  app.ACTIONS.createFullMap = createFullMap
 
-})(window.OM || (window.OM = {}));
+})(window.OM || (window.OM = {}))
--- scripts/run.js
+++ scripts/run.js
@@ -1,31 +1,31 @@
 (function (app) {
-  'use strict';
+  'use strict'
 
-  var initialLevel = 0;
-  var jsonAll = app.DATA.jsonAll = [ app.DATA.jsonMix, app.DATA.jsonSample, app.DATA.jsonComplexArray ];
+  var initialLevel = 0
+  var jsonAll = app.DATA.jsonAll = [ app.DATA.jsonMix, app.DATA.jsonSample, app.DATA.jsonComplexArray ]
   var addToPage = function(data) {
-    var div = document.createElement('div');
-    div.innerHTML = data;
-    div.className = 'kv-item';
-    document.getElementById('writable').appendChild(div);
+    var div = document.createElement('div')
+    div.innerHTML = data
+    div.className = 'kv-item'
+    document.getElementById('writable').appendChild(div)
   }
   var generateData = function(obj) {
     return Object.keys(obj).map(function(key) {
       return app.ACTIONS.createFullMap(obj, key, initialLevel)
-    }).join('');
+    }).join('')
   }
   jsonAll.map(JSON.parse).map(generateData).forEach(function(data) {
-    addToPage(data);
-  });  
+    addToPage(data)
+  })
 
   app.ACTIONS.generate = function () {
-    var json = document.getElementById('json').value;
-    var obj = JSON.parse(json);
-    var writable = document.getElementById('writable');
-    var oldData = writable.innerHTML;
-    var newData = generateData(obj);
-    writable.innerHTML = '';
-    addToPage(newData);
-    addToPage(oldData);
-  };
-})(window.OM || (window.OM = {}));
+    var json = document.getElementById('json').value
+    var obj = JSON.parse(json)
+    var writable = document.getElementById('writable')
+    var oldData = writable.innerHTML
+    var newData = generateData(obj)
+    writable.innerHTML = ''
+    addToPage(newData)
+    addToPage(oldData)
+  }
+})(window.OM || (window.OM = {}))
